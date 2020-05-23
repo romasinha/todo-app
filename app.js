8337ecfccd04e272db4966ad89c5d36d0aad5b10
romasinha <sinha.roma99@gmail.com>
2020-05-23 21:48:06 +0530

    added css

---
+++ app.js
@@ -0,0 +1,38 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.querySelector(".date");
+const list = document.querySelector("#list");
+const input = document.querySelector("#item");
+
+
+function addToDo(toDo){
+    const text = `<li class = "item">
+        <i class="fa fa-circle-thin" aria-hidden="true"></i>
+        <p class = "text"> ${toDo} </p>
+        <i class="fa fa-trash" aria-hidden="true"></i>
+    </li>`
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, text);
+
+}
+
+
+// Adding a todo
+
+const input = document.querySelector("#input");
+input.addEventListener("keyup", function(event){
+
+    if(event.keycode == 13){
+        const toDo = input.value;
+        if(todo){
+            addToDo(todo);
+        }
+    }
+
+        input.value = "";
+
+});
+
+ 
+
